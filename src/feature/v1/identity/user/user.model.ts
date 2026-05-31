9e2539972f1823680ca4f476262b7a873c677c74
Begeeeg <v.yecyec7@gmail.com>
2026-05-31 14:30:45 +0800

    🔥 remove(user): remove unique on username model

--- src/feature/v1/identity/user/user.model.ts
+++ src/feature/v1/identity/user/user.model.ts
@@ -7,7 +7,6 @@ const UserSchema = new Schema<IUser>(
         username: {
             type: String,
             required: true,
-            unique: true,
             trim: true,
             minlength: 3,
         },
