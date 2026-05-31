e876481ad9afd96f067123e68f6677ffa8235c12
Begeeeg <v.yecyec7@gmail.com>
2026-05-31 09:16:09 +0800

    🌟 high(user): signup-data, login-data, and validate-data

---
+++ src/feature/v1/identity/user/dto/signup-data.dto.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const SignupUserSchema = z.object({
+    username: z
+        .string()
+        .min(3, "Username must be at least 3 characters")
+        .max(30),
+
+    email: z.string().email("Invalid email address"),
+
+    password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+export type SignupUserInput = z.infer<typeof SignupUserSchema>;
