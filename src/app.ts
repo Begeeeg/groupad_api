7a142070633fc1a46dea8c7db8b7b167284b4b52
Begeeeg <v.yecyec7@gmail.com>
2026-07-05 09:16:09 +0800

    ♻ ref(app): adding cors for frontend

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { authRouter } from "./feature/v1/identity/auth";
 import { userRouter } from "./feature/v1/identity/user";
 import { listRouter } from "./feature/v1/pad/list";
 import { taskRouter } from "./feature/v1/pad/task";
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
